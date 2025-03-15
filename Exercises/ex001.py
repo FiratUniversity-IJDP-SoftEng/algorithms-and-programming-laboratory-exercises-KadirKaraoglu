85a99364ae1cd439d4c2fdce7e769fac59d37e4b
KadirKaraoglu <169064132+KadirKaraoglu@users.noreply.github.com>
2025-03-15 15:07:28 +0300

    Update ex001.py

--- Exercises/ex001.py
+++ Exercises/ex001.py
@@ -1,5 +1,8 @@
 # Your Student ID:
 # Your Name and Surname:
-import sys
-print("Python version:\n", sys.version)
-print("Version info:\n", sys.version_info)
+print("Twinkle, twinkle, little star,")
+print("\tHow I wonder what you are!") 
+print("\t\tUp above the world so high, ")
+print("\t\tLike a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print("\tHow I wonder what you are")
