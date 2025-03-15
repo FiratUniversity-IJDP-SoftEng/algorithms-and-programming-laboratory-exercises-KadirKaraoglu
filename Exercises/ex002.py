f3513f924da93be0bd98eaef9173ba3ef69dd71a
KadirKaraoglu <169064132+KadirKaraoglu@users.noreply.github.com>
2025-03-15 15:39:42 +0300

    Update ex002.py

--- Exercises/ex002.py
+++ Exercises/ex002.py
@@ -1,2 +1,5 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student 230543005:
+# Your Kadir and Karaoğlu:
+import sys
+print("Python version:\n", sys.version)
+print("Version info:\n", sys.version_info)
