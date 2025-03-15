09a1042eb7170c7a07aff83ab50a23b431a56113
KadirKaraoglu <169064132+KadirKaraoglu@users.noreply.github.com>
2025-03-15 15:57:47 +0300

    ex8

--- Exercises/ex008.py
+++ Exercises/ex008.py
@@ -1,2 +1,9 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student 230543005:
+# Your Kadir and Karaoglu:
+number = (int)(input("Enter a number: "))
+for i in range(number):
+    for j in range(number - i - 1):        
+        print(" ",end="")
+    for j in range(2*i + 1):
+        print("*",end="")
+    print()
