368d411f1d3a36c97e357f2b8bb5c1f761a175b5
KadirKaraoglu <169064132+KadirKaraoglu@users.noreply.github.com>
2025-03-15 15:55:53 +0300

    ex7

--- Exercises/ex007.py
+++ Exercises/ex007.py
@@ -1,2 +1,9 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student 230543005:
+# Your Kadir and Karaoglu:
+w = input("Please enter a text: ")
+print("Character frequencies: ")
+charCount = {}
+for i in word:
+    charCount[i] = charCount.get(i, 0) + 1
+for i in sorted(charCount):
+    print(f"{i}: {charCount[i]}")
