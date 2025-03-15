a7d8ab61bd796a08367a253ada761a9b68056c2b
KadirKaraoglu <169064132+KadirKaraoglu@users.noreply.github.com>
2025-03-15 15:51:48 +0300

    Update ex005.py

--- Exercises/ex005.py
+++ Exercises/ex005.py
@@ -1,2 +1,5 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student 230543005:
+# Your Kadir and Karaoglu:
+firtsname = str (input("Please enter your first name")) 
+lastname = str (input("Please enter your last name"))
+print (f"Hi\t{firtsname}\n  {lastname}")
