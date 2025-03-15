a995f08faaa1a3c9b8a7a7402653a241762199cc
KadirKaraoglu <169064132+KadirKaraoglu@users.noreply.github.com>
2025-03-15 15:40:52 +0300

    Update ex003.py

--- Exercises/ex003.py
+++ Exercises/ex003.py
@@ -1,2 +1,11 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student 230543005:
+# Your Kadir and Karaoğlu:
+import datetime
+today = datetime.date.today()
+print("Current date:", today)
+
+now = datetime.datetime.now()
+print("Current time:",currentTime)
+
+currentTime = now.strftime("%H:%M:%S")
+print(f"Current date and time: {todaysDate} {currentTime}")
